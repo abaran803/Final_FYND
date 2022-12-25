19f9a8d99e6fa1c5c81d948715ce7034394213da
Ayush <abaran803@gmail.com>
2022-12-25 11:29:11 +0530

    CategorySingle Component UI Correction

--- src/pages/CategorySingle.vue
+++ src/pages/CategorySingle.vue
@@ -1,26 +1,14 @@
 <template>
     <div>
-        <!-- Category with Cat_Id: {{$route.params.id}} -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <div class="container mydiv">
-            <div class="row">
-                <router-link v-for="e in items" :to="`/app/product/${e.id}`" :key="e.id" class="col-md-4 mb-3">
-                    <!-- bbb_deals -->
-                    <div class="bbb_deals h-100">
-                        <div class="bbb_deals_slider_container">
-                            <div class=" bbb_deals_item">
-                                <div class="bbb_deals_image"><img :src="e.image" height="300px" alt=""></div>
-                                <div class="bbb_deals_content">
-                                    <div class="bbb_deals_info_line d-flex flex-row justify-content-start">
-                                        <div class="bbb_deals_item_category"><a href="#">{{$route.params.catName}}</a></div>
-                                    </div>
-                                    <div class="bbb_deals_info_line d-flex flex-row justify-content-start">
-                                        <div class="bbb_deals_item_name">{{e.name}}</div>
-                                        <div class="bbb_deals_item_price ml-3">₹{{Math.floor(e.price*81)}}</div>
-                                    </div>
-                                </div>
-                            </div>
+            <div class="row justify-content-center gap-3">
+                <router-link v-for="e in items" :to="`/app/product/${e.id}`" :key="e.id" class="col-md-4 mb-3 text-decoration-none text text-black h-100" style="width: 18rem;">
+                    <div class="card w-100">
+                        <img :src="e.image" height="250px" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ e.name.slice(0, 20) }}... </h5>
+                            <p class="card-text text-muted">{{ $route.params.catName }}</p>
+                            <a href="#" class="btn btn-success">₹ {{ Math.floor(e.price * 81) }}</a>
                         </div>
                     </div>
                 </router-link>
@@ -28,9 +16,6 @@
         </div>
     </div>
 </template>
-
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <script>
 import {getProductByCategoryName} from '../services/api';
@@ -75,203 +60,5 @@ body {
 .mydiv {
     margin-top: 50px;
     margin-bottom: 50px
-}
-
-.cross {
-    font-size: 10px
-}
-
-.padding-0 {
-    padding-right: 5px;
-    padding-left: 5px
-}
-
-.img-style {
-    margin-left: -11px;
-    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    max-width: 104% !important
-}
-
-.m-t-20 {
-    margin-top: 20px
-}
-
-.bbb_background {
-    background-color: #E0E0E0 !important
-}
-
-.ribbon {
-    width: 150px;
-    height: 150px;
-    overflow: hidden;
-    position: absolute
-}
-
-.ribbon span {
-    position: absolute;
-    display: block;
-    width: 34px;
-    border-radius: 50%;
-    padding: 8px 0;
-    background-color: #3498db;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, .1);
-    color: #fff;
-    font: 100 18px/1 'Lato', sans-serif;
-    text-shadow: 0 1px 1px rgba(0, 0, 0, .2);
-    text-transform: uppercase;
-    text-align: center
-}
-
-.ribbon-top-right {
-    top: -10px;
-    right: -10px
-}
-
-.ribbon-top-right::before,
-.ribbon-top-right::after {
-    border-top-color: transparent;
-    border-right-color: transparent
-}
-
-.ribbon-top-right::before {
-    top: 0;
-    left: 17px
-}
-
-.ribbon-top-right::after {
-    bottom: 17px;
-    right: 0
-}
-
-.sold_stars i {
-    color: orange
-}
-
-.ribbon-top-right span {
-    right: 17px;
-    top: 17px
-}
-
-div {
-    position: relative;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box
-}
-
-.bbb_deals_featured {
-    width: 100%
-}
-
-.bbb_deals {
-    width: 100%;
-    margin-right: 7%;
-    padding-top: 25px;
-    padding-left: 25px;
-    padding-right: 25px;
-    padding-bottom: 34px;
-    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    margin-top: 0px
-}
-
-.bbb_deals_title {
-    position: absolute;
-    top: 10px;
-    left: 22px;
-    font-size: 18px;
-    font-weight: 500;
-    color: #000000
-}
-
-.bbb_deals_slider_container {
-    width: 100%
-}
-
-.bbb_deals_item {
-    width: 100% !important
-}
-
-.bbb_deals_image {
-    width: 100%
-}
-
-.bbb_deals_image img {
-    width: 100%
-}
-
-.bbb_deals_content {
-    margin-top: 33px
-}
-
-.bbb_deals_item_category a {
-    font-size: 14px;
-    font-weight: 400;
-    color: rgba(0, 0, 0, 0.5)
-}
-
-.bbb_deals_item_price_a {
-    font-size: 14px;
-    font-weight: 400;
-    color: rgba(0, 0, 0, 0.6)
-}
-
-.bbb_deals_item_price_a strike {
-    color: red
-}
-
-.bbb_deals_item_name {
-    font-size: 24px;
-    font-weight: 400;
-    color: #000000
-}
-
-.bbb_deals_item_price {
-    font-size: 24px;
-    font-weight: 500;
-    color: #6d6e73
-}
-
-.available {
-    margin-top: 19px
-}
-
-.available_title {
-    font-size: 16px;
-    color: rgba(0, 0, 0, 0.5);
-    font-weight: 400
-}
-
-.available_title span {
-    font-weight: 700
-}
-
-@media only screen and (max-width: 991px) {
-    .bbb_deals {
-        width: 100%;
-        margin-right: 0px
-    }
-}
-
-@media only screen and (max-width: 575px) {
-    .bbb_deals {
-        padding-left: 15px;
-        padding-right: 15px
-    }
-
-    .bbb_deals_title {
-        left: 15px;
-        font-size: 16px
-    }
-
-    .bbb_deals_slider_nav_container {
-        right: 5px
-    }
-
-    .bbb_deals_item_name,
-    .bbb_deals_item_price {
-        font-size: 20px
-    }
 }
 </style>
