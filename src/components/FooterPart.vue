ac84e86f18db75edcbaa0d5307fabe3f952c172f
Ayush <abaran803@gmail.com>
2022-12-26 00:22:40 +0530

    Footer Updated

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -1,7 +1,125 @@
 <template>
-    <div class="m-0 bg bg-danger mt-5">
-        <h1 class="m-0 text text-center">Footer Part Component</h1>
+    <!-- Footer -->
+<footer class="text-center text-lg-start bg-white text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+      <!-- Left -->
+      <div class="me-5 d-none d-lg-block">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+  
+      <!-- Right -->
+      <div>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+  
+    <!-- Section: Links  -->
+    <section class="d-none d-md-block">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>Secure Online Exchange
+            </h6>
+            <p>
+              We provide the secure and easy online buy and sell service of reusable products to the normal and advance users.
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Categories
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Category 1</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Category 2</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Category 3</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Category 4</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Others
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Future Plan</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Local Office</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Branches</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Career</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+            <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              info@example.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-4 bg bg-white border" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2022 Copyright:
+      <a class="text-reset fw-bold" href="https://mdbootstrap.com/">SOX.COM</a>
     </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
 </template>
 
 <script>
